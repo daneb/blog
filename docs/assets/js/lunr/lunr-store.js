77fec64cc71e16165ee689a805b25d4dfeab64fa
Dane Balia <daneb@tuta.io>
2021-07-08 16:01:10 +0200

    Rebuild after updating domain

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Better sound: Fiio K3 and Beyerdynamic DT990",
         "excerpt":"As a software developer music has proven an essential aid. From keeping me awake during late night stints to a gentle ushering in the background when focus is called for. I have enjoyed streaming my music through the years and always believed that I was at the pinnacle of my...","categories": ["jekyll","update"],
         "tags": [],
-        "url": "/blog/jekyll/update/2021/05/23/better-sound.html",
+        "url": "/jekyll/update/2021/05/23/better-sound.html",
         "teaser": null
       }]
